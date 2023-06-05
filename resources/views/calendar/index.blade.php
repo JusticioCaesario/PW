09037237a0798946419c5ace09cffd5d2ad44889
salsapsw <salsabillaputerisw29@gmail.com>
2023-06-05 13:44:02 +0700

    ganti judul wkwk

--- resources/views/calendar/index.blade.php
+++ resources/views/calendar/index.blade.php
@@ -41,7 +41,7 @@
     <div class="container">
         <div class="row">
             <div class="col-12">
-                <h3 class="text-center mt-5">FullCalendar js Laravel series with Career Development Lab</h3>
+                <h3 class="text-center mt-5">Kalender Kegiatan Mahasiswa Angkatan 2021</h3>
                 <div class="col-md-11 offset-1 mt-5 mb-5">
 
                     <div id="calendar">
