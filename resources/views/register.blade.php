375eb13b82cbd6257446e38d233e845a0d70d7e6
JusticioCaesario <justicio.caesario@mhs.unsoed.ac.id>
2023-06-07 20:52:35 +0700

    update

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -21,10 +21,6 @@
             <form action="{{route('actionregister')}}" method="post">
             @csrf
                 <div class="form-group">
-                    <label><i class="fa fa-envelope"></i> Email</label>
-                    <input type="email" name="email" class="form-control" placeholder="Email" required="">
-                </div>
-                <div class="form-group">
                     <label><i class="fa fa-user"></i> Username</label>
                     <input type="text" name="username" class="form-control" placeholder="Username" required="">
                 </div>
@@ -38,7 +34,7 @@
                 </div>
                 <button type="submit" class="btn btn-primary btn-block"><i class="fa fa-user"></i> Register</button>
                 <hr>
-                <p class="text-center">Sudah punya akun silahkan <a href="#">Login Disini!</a></p>
+                <p class="text-center">Sudah punya akun silahkan <a href="/">Login Disini!</a></p>
             </form>
         </div>
     </div>
