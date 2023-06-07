d737912db666982a4d6ff7083a26c7d47c58a8f2
marsabilaaa <marsabilaaa@gmail.com>
2023-06-07 21:57:30 +0700

    tampilan create

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -1,56 +1,60 @@
-@if($errors->any()) 
-
-    <div style="color: red">
-        <ul>
-            @foreach($errors->all as $error)
-                <li>{{ $error}}</li>
-            @endforeach
-    
-    @endif
 <!DOCTYPE html>
 <html lang="en">
-@extends('layout')
+    @extends('layout')
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Form Bootstrap</title>
-    
-    <!-- Tautan ke Bootstrap CSS -->
     @section('css')
+    <!-- Tautan ke Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-@endsection
-@section('scripts')
-    <!-- Tautan ke Bootstrap JavaScript dan jQuery -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-@endsection
-
+    <style>
+        .card {
+            margin: 0 auto;
+            margin-top: 50px;
+            width: 400px;
+        }
+    </style>
+    @endsection
 </head>
 @section('content')
 <body>
 
     <div class="container">
-        <form action="/create" method="POST">
-            @csrf
-            <div class="form-group">
-                <label for="namaevent">Nama Event :</label>
-                <input type="text" name="namaevent" class="form-control">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Form Event</h5>
+                @if($errors->any())
+                    <div style="color: red">
+                        <ul>
+                            @foreach($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
+                <form action="/create" method="POST">
+                    @csrf
+                    <div class="form-group">
+                        <label for="namaevent">Nama Event:</label>
+                        <input type="text" name="namaevent" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="deskripsi">Deskripsi:</label>
+                        <textarea name="deskripsi" class="form-control" rows="10"></textarea>
+                    </div>
+                    <div class="form-group">
+                        <label for="tglevent">Tanggal Mulai Event:</label>
+                        <input type="date" name="start_date" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="tglevent">Tanggal Akhir Event:</label>
+                        <input type="date" name="end_date" class="form-control">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Ajukan</button>
+                </form>
             </div>
-            <div class="form-group">
-                <label for="deskripsi">Deskripsi :</label>
-                <textarea name="deskripsi" class="form-control" rows="10"></textarea>
-            </div>
-            <div class="form-group">
-                <label for="tglevent">Tanggal Mulai Event :</label>
-                <input type="date" name="start_date" class="form-control">
-            </div>
-            <div class="form-group">
-                <label for="tglevent">Tanggal Akhir Event :</label>
-                <input type="date" name="end_date" class="form-control">
-            </div>
-            <button type="submit" class="btn btn-primary">Ajukan</button>
-        </form>
+        </div>
     </div>
 
 </body>
