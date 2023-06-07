52778d7eea101dbb32f97f68d8a0e066fe4f73b6
marsabilaaa <marsabilaaa@gmail.com>
2023-06-07 22:56:47 +0700

    show calendar

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -30,6 +30,32 @@ class CalendarController extends Controller
         }
         return view('calendar.index', ['events' => $events]);
     }
+
+    public function show()
+    {
+        $events = array();
+        $bookings = Booking::all();
+        foreach($bookings as $booking) {
+            $color = null;
+            if($booking->title == 'penting') {
+                $color = '##FF699A';
+            }
+            if($booking->title == 'Test 1') {
+                $color = '#68B01A';
+            }
+
+            $events[] = [
+                'id'   => $booking->id,
+                'title' => $booking->title,
+                'start' => $booking->start_date,
+                'end' => $booking->end_date,
+                'color' => $color
+            ];
+        }
+        return view('calendar.show', ['events' => $events]);
+    }
+
+
     public function store(Request $request)
     {
         $request->validate([
--- routes/web.php
+++ routes/web.php
@@ -28,10 +28,13 @@ Route::get('/users', [UserController::class, 'index'])->name('users.index');
 Route::get('/test', [UserController::class, 'test']);
 
 // Calendar routes
+
 Route::get('calendar/index', [CalendarController::class, 'index'])->name('calendar.index');
 Route::post('calendar', [CalendarController::class, 'store'])->name('calendar.store');
 Route::patch('calendar/update/{id}', [CalendarController::class, 'update'])->name('calendar.update');
 Route::delete('calendar/destroy/{id}', [CalendarController::class, 'destroy'])->name('calendar.destroy');
+//INI ROUTE TAMPIL KALENDER BBUAT HOME NNTI
+Route::get('calendar/show', [CalendarController::class, 'show'])->name('calendar.show');
 
 Route::get('books', [BookController::class, 'index'])->name('books.index');
 Route::post('books', [BookController::class, 'store'])->name('books.store');
